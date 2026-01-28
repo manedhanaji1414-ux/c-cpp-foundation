d7978ff4feb8bca4b117007401c474b859a6743a
Dhanaji Mane <manedhanaji1414@gmail.com>
2026-01-28 23:58:55 +0530

    Added practice code of variables

---
+++ Test1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int Myage = 22;
+	char FLetter = 'D';
+	double Weight = 58.5200;
+	string Name = "Dhanaji Mane";
+
+	cout <<"My Name is:"<< Name <<"\n";
+	cout << "My age is:"<< Myage <<"\n";
+	cout << "My weight is:" << Weight << "\n";
+	cout << "First character of my name is :"<< FLetter <<endl;
+
+}
